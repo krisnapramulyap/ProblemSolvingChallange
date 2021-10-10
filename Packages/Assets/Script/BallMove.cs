6af8345d1f74b432a80a20549e55ad841e3cc483
Krisna Pramulya Putra <19560029@stmik.jayakarta.ac.id>
2021-10-10 22:29:44 +0700

    Add Package Problem 5

---
+++ Packages/Assets/Script/BallMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    // header
+    [Header("Ball Movement")]
+    public float speed = 6f;  // speed for ball
+
+    Rigidbody2D playerRigidbody;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+
+
+    public void Move(float h, float v)
+    {
+        Vector3 temVect = new Vector3(h, v, 0);
+        temVect = temVect.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(playerRigidbody.transform.position + temVect);
+    }
+
+}
---
+++ Packages/Assets/Script/MouseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    private Rigidbody2D playerRigidbody;
+    public Camera mainCamera;
+
+    // Header
+    [Header("Ball Movement")]
+    public Vector2 ballAngle = new Vector2(100, 100);
+    public float speed;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 target = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        Vector3 moveAngle = target - transform.position;
+
+        if (Vector3.Distance(target, transform.position) > 0.5)
+            transform.Translate(moveAngle.normalized * speed * Time.deltaTime);
+    }
+}
