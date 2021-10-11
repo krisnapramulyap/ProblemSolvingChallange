3936b38968f6847eadd95c1faf061267b1702a16
Krisna Pramulya Putra <19560029@stmik.jayakarta.ac.id>
2021-10-11 20:43:47 +0700

    Add Package Problem 7

---
+++ Assets/Script/Kubus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Kubus : MonoBehaviour
+{
+    public float Health = 50f;
+
+    public UnityAction<GameObject> OnEnemyDestroyed = delegate { };
+
+    private bool _isHit = false;
+
+    void OnDestroy()
+    {
+        if (_isHit)
+        {
+            OnEnemyDestroyed(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PengaturanScore.Instance.IncrementScore();
+            this.gameObject.SetActive(false);
+            Destroy(this.gameObject, 1);
+        }
+    }
+
+}
---
+++ Assets/Script/PengaturanScore.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PengaturanScore : MonoBehaviour
+{
+    #region singleton
+
+    private static PengaturanScore _instance;
+
+    public static PengaturanScore Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<PengaturanScore>();
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    private static int score;
+    public Text scoreText;
+
+    private void Awake()
+    {
+        score = 0;
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score : " + score;
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+    }
+}
