b9a74b7c4252375b76f91414b57675e970cb7494
Krisna Pramulya Putra <19560029@stmik.jayakarta.ac.id>
2021-10-12 22:39:25 +0700

    Add Package Problem 10

---
+++ Assets/Script/MenuChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuChange : MonoBehaviour
+{
+    private void Update()
+    {
+        string scenename = SceneManager.GetActiveScene().name;
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (scenename != "ProblemSolving10")
+            {
+                SceneManager.LoadScene("ProblemSolving10");
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void gantiscene(string scene){
+        SceneManager.LoadScene(scene);
+    }
+
+    public void exitgame(){
+        Application.Quit();
+
+    }
+
+    
+    
+}
